f5d813c971b5130566e25a61a084d317f392f742
ethanwatsonj88 <ethanwatsonj88@gmail.com>
2019-02-12 22:59:20 -0500

    started fleshing out game.ex

--- lib/project1/game.ex
+++ lib/project1/game.ex
@@ -11,27 +11,30 @@ defmodule Project1.Game do
 	#		p1 moves first
 	#		sends action
 
-
-
 	# data structures
 	# p1name - string that user types in
 	# p2name - string that user types in
 	# p1cards - list of cards player has
 	# p2cards - list of cards player has
 	# card - map of attributes of a card
-			%{ :name => "name", :actions => moves }
+	#		%{ :name => "name", :health => 10, :moves => moves }
 	# moves - map of move name and damage
-			%{ :name => "name", :damage => 10 }
+	#		%{ :name => "name", :damage => 10 }
+
+	# functions called from react
+	# action(player,
 
 	def new do
 		%{
+			# new should be passed in the two names
 			p1name: "",
 			p2name: "",
-			p1cards:[],
-			p2cards:[],
+			p1cards: p1Cards(),
+			p2cards: p2Cards(),
 
 			p1currCard: nil,
 			p2currCard: nil,
+			isGameOver: false,
 		}
 	end
 
@@ -39,7 +42,21 @@ defmodule Project1.Game do
 
 	end
 
+	def p1Cards() do
+		move1 = %{ :name => "punch", :damage => 10 }
+		card1 = %{ :name => "pikachu", :health => 20, :moves => [move1] }
+		[card1]
+	end
+
+	def p2Cards() do
+		move1 = %{ :name => "kick", :damage => 10 } 
+		card1 = %{ :name => "macho", :health => 40, :moves => [move1] }
+		[card1] 
+	end
 end
+
+
+
 
 
 # can implement later:
