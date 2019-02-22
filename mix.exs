bff6714149ee04b5df841345dc9a4b8c574aa9e3
ethanwatsonj88 <ethanwatsonj88@gmail.com>
2019-02-21 20:29:40 -0500

    distillery

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Project1.MixProject do
   def project do
     [
       app: :project1,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -38,7 +38,8 @@ defmodule Project1.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+			{:distillery, "~> 2.0"}
     ]
   end
 end
